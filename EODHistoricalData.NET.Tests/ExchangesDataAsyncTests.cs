bae2922f71da30c336855ed15f23d4dc36693da5
Sven <radi@mxmate.com>
2022-02-16 10:13:28 +0100

    Added option to fetch exchange details from API Added corresponding test cases

--- EODHistoricalData.NET.Tests/ExchangesDataAsyncTests.cs
+++ EODHistoricalData.NET.Tests/ExchangesDataAsyncTests.cs
@@ -1,4 +1,5 @@
 ﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
 using System.Threading.Tasks;
 
 namespace EODHistoricalData.NET.Tests
@@ -13,6 +14,64 @@ namespace EODHistoricalData.NET.Tests
             var exchanges = await client.GetExchangeListAsync();
             Assert.IsNotNull(exchanges);
             Assert.IsNotNull(exchanges.Count > 50);
+        }
+
+        [TestMethod]
+        public async Task exchange_details_returns_data_no_date_async()
+        {
+            using var client = new EODHistoricalDataAsyncClient(Consts.ApiToken, true);
+            var exchangeDetails = await client.GetExchangeDetailsAsync("US", null, null);
+            Assert.IsNotNull(exchangeDetails);
+            Assert.IsTrue(exchangeDetails.ExchangeHolidays.Count > 0);
+        }
+
+        [TestMethod]
+        public async Task exchange_details_returns_data_start_date_async()
+        {
+            using var client = new EODHistoricalDataAsyncClient(Consts.ApiToken, true);
+            DateTime startDate = DateTime.Now;
+
+            var exchangeDetails = await client.GetExchangeDetailsAsync("US", startDate, null);
+            Assert.IsNotNull(exchangeDetails);
+            Assert.IsTrue(exchangeDetails.ExchangeHolidays.Count > 0);
+
+            foreach (ExchangeHoliday holiday in exchangeDetails.ExchangeHolidays.Values)
+            {
+                Assert.IsTrue(holiday.Date >= startDate);
+            }
+        }
+
+        [TestMethod]
+        public async Task exchange_details_returns_data_end_date_async()
+        {
+            using var client = new EODHistoricalDataAsyncClient(Consts.ApiToken, true);
+            DateTime endDate = DateTime.Now;
+
+            var exchangeDetails = await client.GetExchangeDetailsAsync("US", null, endDate);
+            Assert.IsNotNull(exchangeDetails);
+            Assert.IsTrue(exchangeDetails.ExchangeHolidays.Count > 0);
+
+            foreach (ExchangeHoliday holiday in exchangeDetails.ExchangeHolidays.Values)
+            {
+                Assert.IsTrue(holiday.Date <= endDate);
+            }
+        }
+
+        [TestMethod]
+        public async Task exchange_details_returns_data_start_end_date_async()
+        {
+            using var client = new EODHistoricalDataAsyncClient(Consts.ApiToken, true);
+            DateTime startDate = DateTime.Now.Subtract(TimeSpan.FromDays(180));
+            DateTime endDate = DateTime.Now.Add(TimeSpan.FromDays(180));
+
+            var exchangeDetails = await client.GetExchangeDetailsAsync("US", startDate, endDate);
+            Assert.IsNotNull(exchangeDetails);
+            Assert.IsTrue(exchangeDetails.ExchangeHolidays.Count > 0);
+
+            foreach (ExchangeHoliday holiday in exchangeDetails.ExchangeHolidays.Values)
+            {
+                Assert.IsTrue(holiday.Date >= startDate && holiday.Date <= endDate);
+            }
         }
     }
 }
